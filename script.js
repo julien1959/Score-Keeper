2c7b2f378428705d6c6697bce28b029882a1db07
Julien <dev.julien.f@gmail.com>
2019-09-29 17:40:36 +0200

    creation function score winner in script.js

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const buttonReset = document.querySelectorAll("button")[2];
 
 let scoreP1 = 0
 let scoreP2 = 0
-let scoreMax = 5
+let scoreMax = 0
 
 //  PLAYER 1
 
 function insertScoreP1IntoSpan0() {
-    scorePlayer1.insertAdjacentText("afterbegin", scoreP1);
+    scorePlayer1.append(document.createTextNode(scoreP1));
 }
 
 function displayScorePlayer1() {
@@ -29,7 +29,7 @@ function displayScorePlayer1() {
 // PLAYER 2
 
 function insertScoreP2IntoSpan1() {
-    scorePlayer2.insertAdjacentText("afterbegin", scoreP2);
+    scorePlayer2.append(document.createTextNode(scoreP2));
 }
 
 function displayScorePlayer2() {
@@ -44,8 +44,24 @@ function displayScorePlayer2() {
 function scoreReset() {
     scorePlayer1.textContent = (scoreP1 = 0);
     scorePlayer2.textContent = (scoreP2 = 0);
+    input.value = NaN;
+    
 }
+
+// SCORE WINNER
+
+function insertValueInputIntoSpan2() {
+    
+    scoreWinner.append(document.createTextNode(input.value));
+    scoreWinner.textContent = input.value;
+    scoreMax = input.value;
+       
+
+}
+
+
 
 buttonPlayer1.addEventListener("click", displayScorePlayer1);
 buttonPlayer2.addEventListener("click", displayScorePlayer2);
 buttonReset.addEventListener("click", scoreReset);
+input.addEventListener("click", insertValueInputIntoSpan2);
