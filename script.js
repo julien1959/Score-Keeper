2b207851ba5a5d88e1b4c060f01463d3317f211a
Julien <dev.julien.f@gmail.com>
2019-09-29 15:41:08 +0200

    creation function Player 1

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ const buttonPlayer1 = document.querySelectorAll("button")[0];
 const buttonPlayer2 = document.querySelectorAll("button")[1];
 const buttonReset = document.querySelectorAll("button")[2];
 
+let scoreP1 = 0
+let scoreP2 = 0
+let scoreMax = 5
+
+//  PLAYER 1
+
+function insertScoreP1IntoSpan0() {
+    scorePlayer1.insertAdjacentText("afterbegin", scoreP1);
+}
+
+function modifyScorePlayer1() {
+    if (scoreP1 < scoreMax) {
+        scoreP1++
+        scorePlayer1.textContent = scoreP1;
+    }
+}
+
+// PLAYER 2
+
+function insertScoreP2IntoSpan1() {
+    scorePlayer2.insertAdjacentText("afterbegin", scoreP2);
+}
+
+function modifyScorePlayer2() {
+    if (scoreP2 < scoreMax) {
+        scoreP2++
+        scorePlayer2.textContent = scoreP2;
+    }
+}
+
+buttonPlayer1.addEventListener("click", modifyScorePlayer1);
+buttonPlayer2.addEventListener("click", modifyScorePlayer2);
