319d5b006844772ab20feb90db580cd1ace1cbc6
Julien <dev.julien.f@gmail.com>
2019-09-29 11:06:17 +0200

    creation of variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const h1 = document.querySelector("h1");
+const input = document.querySelector("input");
+const scorePlayer1 = document.querySelectorAll("span")[0];
+const scorePlayer2 = document.querySelectorAll("span")[1];
+const scoreWinner = document.querySelectorAll("span")[2];
+const buttonPlayer1 = document.querySelectorAll("button")[0];
+const buttonPlayer2 = document.querySelectorAll("button")[1];
+const buttonReset = document.querySelectorAll("button")[2];
+
